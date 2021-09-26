ece08e6de7d2cf5cdf4763c1e08e74c6c29f7c65
whitexiong <986247535@qq.com>
2021-09-26 14:24:46 +0800

    新增qs工具， 完成搜索功能

--- src/screens/prjiect-list/index.jsx
+++ src/screens/prjiect-list/index.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import {SearchPanel} from "./seach-panel";
 import {List} from "./list";
 import {useEffect, useState} from "react";
+import qs from "qs";
+import {clearObject} from "../../utils";
 
 
 const apiUrl = process.env.REACT_APP_API_URL
 export const ProjectListScreen = () => {
 
+    //状态提升
     const [users, setUsers] = useState([])
     const [param, setParam] = useState({
         name: '',
@@ -16,7 +19,7 @@ export const ProjectListScreen = () => {
 
     //发生变化的请求接口
     useEffect(() => {
-        fetch(`${apiUrl}/projects`).then(async response => {
+        fetch(`${apiUrl}/projects?${qs.stringify(clearObject(param))}`).then(async response => {
             if (response.ok) {
                 setList(await response.json())
             }
